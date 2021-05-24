be97346ddee447d21fa4b17d6b30af0a3820a003
mehdi <mehdimotaqi@gmail.com>
2021-05-25 00:24:59 +0100

    - Create Either file to manage data state

--- app/src/main/java/com/gemography/coding/challenge/data/network/GithubApi.kt
+++ app/src/main/java/com/gemography/coding/challenge/data/network/GithubApi.kt
@@ -1,5 +1,7 @@
 package com.gemography.coding.challenge.data.network
 
+import com.gemography.coding.challenge.data.network.entities.RepositoryResponse
+import io.reactivex.Observer
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,6 +13,6 @@ interface GithubApi {
         @Query(QueriesGithubApiConstant.SORT) sort: String,
         @Query(QueriesGithubApiConstant.ORDER) order: String,
         @Query(QueriesGithubApiConstant.PAGE) page: Int
-    )
+    ): Observer<RepositoryResponse>
 
 }
