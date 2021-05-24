87050ead793671d314d7a3b1b9e19aa37e2bc43d
mehdi <mehdimotaqi@gmail.com>
2021-05-25 00:20:14 +0100

    - Create Github Object Response (GithubApiRepository.kt)

---
+++ app/src/main/java/com/gemography/coding/challenge/data/network/entities/GithubApiRepositoryResponse.kt
@@ -0,0 +1,18 @@
+package com.gemography.coding.challenge.data.network.entities
+
+data class RepositoryResponse(
+    val total_count: Int,
+    val incomplete_results: Boolean,
+    val items: RepositoryItem
+)
+
+data class RepositoryItem(
+    val name: String,
+    val owner: RepositoryOwner,
+    val description: List<String>
+)
+
+data class RepositoryOwner(
+    val login: String,
+    val avatar_url: List<String>
+)
